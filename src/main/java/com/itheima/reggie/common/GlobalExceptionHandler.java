c2792ad84cf1af409e02883af99cebdd1e7a9ec1
ctl <1198924667@qq.com>
2023-02-11 19:36:12 +0800

    Day2 1：登录的功能完善 2：查询员工 3：员工状态修改

---
+++ src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.itheima.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class}) // 拦截加了RestController、Controller注解的所有异常
+@ResponseBody
+@Slf4j // 输出日志
+public class GlobalExceptionHandler {
+    /**
+     * 异常处理方法
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+        if(ex.getMessage().contains("Duplicate entry")){  // 异常信息中含有"Duplicate entry"
+            String[] split = ex.getMessage().split(" "); // 以空格分割开
+            String msg = split[2] + "已存在";
+            return R.error(msg);//返回错误内容
+        }
+        return R.error("未知错误");
+    }
+}
+
--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,18 +1,17 @@
 package com.itheima.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -73,10 +72,55 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee"); // 清理保存的id
         return R.success("退出成功");
     }
-    /*@PostMapping("/logout")
-    public R<String> logout(HttpServletRequest request){
-        //清理Session中保存的当前登录员工的id
-        request.getSession().removeAttribute("employee");
-        return R.success("退出成功");
-    }*/
+    @PostMapping()
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee){
+        //1、服务端Controller接收页面提交的数据并调用Service将数据进行保存
+        log.info("新增员工， 员工信息：{}", employee.toString());
+        //设置初始密码123456需要MD5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());  // 记录创建时间
+        employee.setUpdateTime(LocalDateTime.now());  // 记录更新时间
+        Long empID = (Long) request.getSession().getAttribute("employee");  //获得当前登录用户id，强转
+        employee.setCreateUser(empID);  //设置是谁创建的用户
+        employee.setUpdateUser(empID);  //设置是谁更新的用户
+
+        //2、Service调用Mapper操作数据库，保存数据
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
+
+    /**
+     * 员工信息分页查询
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        log.info("page = {}, pageSize = {}, name = {}", page, pageSize, name);
+        // 1、构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        // 2、构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        // 过滤条件1：名字
+        if(name != null){
+            queryWrapper.like(Employee::getName, name);
+        }
+        // 排序条件：更新时间
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        // 3、执行查询
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据ID修改员工信息
+     */
+    @PutMapping
+    public R<String> updata(HttpServletRequest request, @RequestBody Employee employee){
+        log.info(employee.toString());
+        employee.setUpdateTime(LocalDateTime.now());  // 记录修改员工的时间
+        Long empId = (Long)request.getSession().getAttribute("employee");  // 获取当前用户
+        employee.setUpdateUser(empId);  // 记录修改员工的用户
+        employeeService.updateById(employee);
+        return null;
+    }
 }
