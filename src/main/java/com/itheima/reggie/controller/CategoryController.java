f4520f0c5bf1c9309b07b4b880aabace25133f5a
tcp <t549946327@163.com>
2023-02-13 17:19:30 +0800

    分类模块：分页查询、删除、修改功能

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理
@@ -32,4 +31,41 @@ public class CategoryController {
         return R.success("新增分类成功");
     }
 
+    /**
+     * 分类分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        Page pagInfo = new Page(page,pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pagInfo,queryWrapper);
+        return R.success(pagInfo);
+    }
+
+    /**
+     * 根据id对分类信息进行删除
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete (Long id){
+        log.info("删除分类的id为：{}",id);
+        categoryService.remove(id);
+        return R.success("分类信息删除成功");
+    }
+
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Category category){
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功！");
+    }
 }
--- src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,44 @@
 package com.itheima.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
 import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.Setmeal;
 import com.itheima.reggie.mapper.CategoryMapper;
 import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.DishService;
+import com.itheima.reggie.service.SetmealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private SetmealService setmealService;
+    /**
+     * 根据id删除分类，删除前需要进行判断
+     * @param id
+     */
+    @Override
+    public void remove(Long id) {
+        LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        LambdaQueryWrapper<Setmeal> setmealLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //查询当前分类是否关联菜品和套餐
+        dishLambdaQueryWrapper.eq(Dish::getCategoryId,id);
+        int dish_count = dishService.count(dishLambdaQueryWrapper);
+        setmealLambdaQueryWrapper.eq(Setmeal::getCategoryId,id);
+        int setmeal_count = setmealService.count(setmealLambdaQueryWrapper);
+        if(dish_count>0){
+            throw new CustomException("当前分类关联了菜品，不能删除！");
+        }
+        if(setmeal_count>0){
+            throw new CustomException("当前分类关联了套餐，不能删除！");
+        }
+        super.removeById(id);
+    }
 }
